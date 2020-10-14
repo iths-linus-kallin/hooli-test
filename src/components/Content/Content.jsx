83477410bcd539b8d2276a7d21a47102ef8e2d49
Linus Kallin <jaglinus@gmail.com>
2020-10-14 11:50:42 +0200

    basic structure

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,9 +4,13 @@ import "./Content.scss";
 const Content = () => {
   return (
     <div className="Content">
-      <h1 className="Content__Title"></h1>
-      <p className="Content__Text"></p>
-      <img className="Content__Image" src="" alt=""/>
+      <h1 className="Content__Title">
+        Content
+      </h1>
+      <p className="Content__Text">
+        Text
+      </p>
+      <img className="Content__Image" src="" alt="image1"/>
     </div>
   )
 }
