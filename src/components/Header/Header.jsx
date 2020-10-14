f42a9f0d8edfde1378ed71c2af0361b4c876116f
Jessica <jessica.schwabegger@hotmail.com>
2020-10-14 11:29:02 +0200

    Add purebem for naming classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import "./Header.scss";
+import {BrowserRouter as Router, Route, Switch, NavLink} from "react-router-dom";
+import purebem from 'purebem';
 
 const Header = () => {
+  const block = purebem("header");
+
   return (
-    <div className="Header">
-      Header
+    <div className={ block() }>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <header>
+            <nav className={ block("nav-section") }>
+              <NavLink to="/strength" activeClassName="active"> Vår styrka </NavLink>
+              <NavLink to="/wishes" activeClassName="active"> Önskemål </NavLink>
+              <NavLink to="/flaws" activeClassName="active"> Brister </NavLink>
+              <NavLink to="/work" activeClassName="active"> Arbetssätt </NavLink>
+            </nav>
+          </header>
+        </Route>
+      </Switch>
+    </Router>
     </div>
   )
 }
