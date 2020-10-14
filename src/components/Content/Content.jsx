fba20e75959adb64905a828e5af6f0b4880a01fb
Linus Kallin <jaglinus@gmail.com>
2020-10-14 11:17:14 +0200

    added elements

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,7 +4,9 @@ import "./Content.scss";
 const Content = () => {
   return (
     <div className="Content">
-      Content
+      <h1 className="Content__Title"></h1>
+      <p className="Content__Text"></p>
+      <img className="Content__Image" src="" alt=""/>
     </div>
   )
 }
