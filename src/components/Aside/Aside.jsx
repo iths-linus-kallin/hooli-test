d87d93d3b6e032c092bd37cd0b2063a1c3956d37
iths-jakob-gauffin <54362410+iths-jakob-gauffin@users.noreply.github.com>
2020-10-14 15:42:46 +0200

    fix employeeobject and start dataobj for content

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import "./Aside.scss";
 
 const Employee = ({employee}) => {
-  console.log("OUTPUT ÄR: Employee -> employee");
+    
+console.log("Employee -> employee", employee)
 
   return (
     <li className="Employee">
@@ -22,29 +23,29 @@ const Employee = ({employee}) => {
 
 const Aside = () => {
   const employees = [
-    {linus: {
+    {
         name: "Linus Kallin",
         img: "./images/portraits/linus.jpg",
         description: "Professionell gamblare",
         id: 1,
         alt: "Bild på Linus"
-    }},
-    {jakob: {
+    },
+    {
       name: "Jakob Gauffin",
       // img: "jakob",
       img: "images/portraits/linus.jpg",
       description: "Professionell bagare",
       id: 2,
       alt: "Bild på Linus",
-    }},
-    {jessica: {
+    },
+    {
       name: "Jessica Schwabegger",
       img: "images/portraits/linus.jpg",
       // img: "jessica",
       description: "Professionell kodare",
       id: 3,
       alt: "Bild på Linus"
-    }}
+    }
   ];
 
   return (
@@ -54,19 +55,18 @@ const Aside = () => {
 
       <section className="Aside__Employees">
 
-        {/* <ul className="Aside__List">
-          {employees && employees.map(x =>{
-          {console.log("OUTPUT ÄR: Aside -> x", x.id);}
+        <ul className="Aside__List">
+          {employees && employees.map(x =>
           
-          return (
-            <div className="Aside__EmployeeWrapper" key={x.id}>
+        //   return (
+            // <div className="Aside__EmployeeWrapper" key={x.id}>
               <Employee employee={x}/>
-            </div>
-          )
+            // </div>
+        //   )
             
-          } 
+          
             )}
-        </ul> */}
+        </ul>
       
         <div className="Aside__Meta">
           <h5 className="Aside__MetaTitle">
--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,5 @@
 const textData = {
-    strength: {
+    strengths: {
         title: "Vår styrka",
         text: `Vi på Hooli brinner för att konstruera flexibla och användarvänliga gränssnitt som i sin grund vilar på en robust och skalbar kodbas. De teknologier vi använder i utvecklingsfas är HTML, CSS-preprocessorn SCSS och Javascript. React är primärt det frontendramverk vi arbetar med, samt tar fram strömformade API-lösningar med Node.js och Express som vi knyter till dokumentdatabassystem såsom Firebase.
         Dessutom är vi konkurrenskraftiga inom UX-design och PHP, men även inom andra medier såsom videoredigering och musikproduktion. 
@@ -7,5 +7,14 @@ const textData = {
         Välkommen!
         /Vännerna på Hooli
         `
-    }
+    },
+    wishes: {
+
+    },
+    flaws: {
+
+    },
+    works: {
+
+    },
 };
