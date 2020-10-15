5e1b5235f4c6f8322fc45f8048dabfd884af0846
Jakob <jakob_gauffin@hotmail.com>
2020-10-15 21:16:33 +0200

    Minor fixes in Main branch

--- src/data/data.js
+++ src/data/data.js
@@ -1,27 +1,20 @@
 export const textData = {
     strengths: {
         title: "Vår styrka",
-        text: `Vi på Hooli brinner för att konstruera flexibla och användarvänliga gränssnitt som i sin grund vilar på en robust och skalbar kodbas. De teknologier vi använder i utvecklingsfas är HTML, CSS-preprocessorn SCSS och Javascript. React är primärt det frontendramverk vi arbetar med, samt tar fram strömformade API-lösningar med Node.js och Express som vi knyter till dokumentdatabassystem såsom Firebase.
-        Dessutom är vi konkurrenskraftiga inom UX-design och PHP, men även inom andra medier såsom videoredigering och musikproduktion. 
-        Så ta steget - bli Hoolipartner idag och du kommer fram över alltid att vara evigt lycklig och aldrig någonsin stöta på ett problem igen i hela ditt liv. 
-        Välkommen!
-        /Vännerna på Hooli
-        `
+        text: "Vi på Hooli brinner för att konstruera flexibla och användarvänliga gränssnitt som i sin grund vilar på en robust och skalbar kodbas. \n\nDe teknologier vi använder i utvecklingsfas är HTML, CSS-preprocessorn SCSS och Javascript. \n\nReact är primärt det frontendramverk vi arbetar med, samt tar fram strömformade API-lösningar med Node.js och Express som vi knyter till dokumentdatabassystem såsom Firebase. \nDessutom är vi konkurrenskraftiga inom UX-design och PHP, men även inom andra medier såsom videoredigering och musikproduktion. \n\nSå ta steget - bli Hoolipartner idag och du kommer för alltid att vara evigt lycklig och aldrig någonsin stöta på ett problem igen i hela ditt liv. \n\nVälkommen! \n\n/Vännerna på Hooli",
     },
     wishes: {
-        title: "Önskningar",
-        text: `Vi tycker det skulle vara kul att arbeta med Wordpress och dess plug-ins. Vi tänker att en Webbshop är ett bra projekt att sätta tänderna i eftersom där finns många möjligheter att införa olika funktionaliteter: en shoppingkorg, användarinloggning, kundkommentarsfält, produktbetygssystem o.s.v. Tekniker som vi tänker oss kan lära oss mycket.`
+        title: "Önskemål",
+        text: `Vi tycker det skulle vara kul att arbeta med Wordpress och dess plug-ins. \n\nVi tänker att en Webbshop är ett bra projekt att sätta tänderna i eftersom där finns många möjligheter att införa olika funktionaliteter: en shoppingkorg, användarinloggning, kundkommentarsfält, produktbetygssystem o.s.v. \nDet är tekniker som vi tänker oss kan lära oss mycket.`
     },
     flaws: {
         title: "Brister",
-        text: `Vi har verkligen fått tänka hårt och länge för att komma på något där Hoolis expertis inte skulle vara fullkomligt felfri och ”cutting edge”. Men sen kom vi på att vi har glömt en hel del PHP. Vi har inte heller jobbat särskilt mycket i Wordpress. 
-        Men förhoppningsvis kan vi ändra på detta när vi bekantar oss med Wordpress då vi hoppas att PHP-kunskaperna kommer tillbaka. 
+        text: `Vi har verkligen fått tänka hårt och länge för att komma på något där Hoolis expertis inte skulle vara fullkomligt felfri och ”cutting edge”. \n\nMen sen kom vi på att vi har glömt en hel del PHP. \n\nVi har inte heller jobbat särskilt mycket i Wordpress. \n\nMen förhoppningsvis kan vi ändra på detta när vi bekantar oss med Wordpress då vi hoppas att PHP-kunskaperna kommer tillbaka. 
         `
     },
     works: {
         title: "Arbetssätt",
-        text: `Hoolis arbetsdagar startar med ett virtuellt möte med alla kollegor. Det här uppstartsmötet äger rum kl. 9 på Teams varje måndag – torsdag. På fredagar är tanken att man ses IRL, på skolan eller på annan plats, för att samla upp veckans genomförda arbete och göra en översiktsplanering för kommande vecka.  
-        Givetvis har vi alla löpande kontakt på Teams under arbetstid om frågor skulle uppstå. 
+        text: `Hoolis arbetsdagar startar med ett virtuellt möte med alla kollegor. \n\nDet här uppstartsmötet äger rum kl. 9 på Teams varje måndag – torsdag. \n\nPå fredagar är tanken att man ses IRL, på skolan eller på annan plats, för att samla upp veckans genomförda arbete och göra en översiktsplanering för kommande vecka.\n\nGivetvis har vi alla löpande kontakt på Teams under arbetstid om frågor skulle uppstå. 
         `
     },
 };
